c5e2ae0ffdeb5d732980ac1409bbc4ee450b6638
e7d <michael@e7d.io>
2022-12-02 19:17:14 +0100

    detailed handling of each connection types (connect, file transfer, port forward)

--- extension.js
+++ extension.js
@@ -18,7 +18,7 @@
 
 
 const ByteArray = imports.byteArray;
-const { GLib, GObject, St } = imports.gi;
+const { Clutter, GLib, GObject, St } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
@@ -29,7 +29,9 @@ const { gettext } = ExtensionUtils;
 
 const RUSTDESK_SERVICE_PID_REGEXP = /^\w+ +(?<PID>\d+).*rustdesk --service$/;
 const RUSTDESK_MAIN_PID_REGEXP = /^\w+ +(?<PID>\d+).*rustdesk$/;
-const RUSTDESK_SESSION_PID_REGEXP = /^\w+ +(?<PID>\d+).*rustdesk --connect (?<sessionID>\d+)$/;
+const RUSTDESK_SESSION_CONNECT_PID_REGEXP = /^\w+ +(?<PID>\d+).*rustdesk --connect (?<sessionID>\d+)$/;
+const RUSTDESK_SESSION_FILE_TRANSFER_PID_REGEXP = /^\w+ +(?<PID>\d+).*rustdesk --file-transfer (?<sessionID>\d+)$/;
+const RUSTDESK_SESSION_PORT_FORWARD_PID_REGEXP = /^\w+ +(?<PID>\d+).*rustdesk --port-forward (?<sessionID>\d+)$/;
 const WINDOW_STATE_REGEXP = /window state: (?<state>\w+)$/;
 
 function execCommand(cmd) {
@@ -37,7 +39,7 @@ function execCommand(cmd) {
     let [, stdout, stderr, status] = GLib.spawn_command_line_sync(cmd);
     if (status !== 0) {
       if (stderr instanceof Uint8Array) stderr = ByteArray.toString(stderr);
-      throw new Error(stderr);
+      throw new Error(`Cmd failed: ${cmd}\nError detail: ${stderr}`);
     }
     if (stdout instanceof Uint8Array) stdout = ByteArray.toString(stdout);
     return stdout;
@@ -45,6 +47,10 @@ function execCommand(cmd) {
     logError(e);
     return '';
   }
+}
+
+function toSession(sessions, sessionID) {
+  return sessions[sessionID] || { sessionID, connect: {}, fileTransfer: {}, portForward: {} }
 }
 
 function parseProcesses() {
@@ -62,32 +68,62 @@ function parseProcesses() {
       const windowState = windowID && getWindowState(windowID);
       processes.main = { PID, windowID, windowState };
     }
-    const sessionMatches = RUSTDESK_SESSION_PID_REGEXP.exec(line);
-    if (sessionMatches) {
-      const { PID, sessionID } = sessionMatches.groups;
+    const sessionConnectMatches = RUSTDESK_SESSION_CONNECT_PID_REGEXP.exec(line);
+    if (sessionConnectMatches) {
+      const { PID, sessionID } = sessionConnectMatches.groups;
       const windowID = PID && findWindowID(PID);
       const windowState = windowID && getWindowState(windowID);
-      processes.sessions.push({ PID, sessionID, windowID, windowState });
+      processes.sessions[sessionID] = {
+        ...toSession(processes.sessions, sessionID),
+        connect: { PID, windowID, windowState }
+      };
+    }
+    const sessionFileTransferMatches = RUSTDESK_SESSION_FILE_TRANSFER_PID_REGEXP.exec(line);
+    if (sessionFileTransferMatches) {
+      const { PID, sessionID } = sessionFileTransferMatches.groups;
+      const windowID = PID && findWindowID(PID);
+      const windowState = windowID && getWindowState(windowID);
+      processes.sessions[sessionID] = {
+        ...toSession(processes.sessions, sessionID),
+        fileTransfer: { PID, windowID, windowState }
+      };
+    }
+    const sessionPortForwardMatches = RUSTDESK_SESSION_PORT_FORWARD_PID_REGEXP.exec(line);
+    if (sessionPortForwardMatches) {
+      const { PID, sessionID } = sessionPortForwardMatches.groups;
+      const windowID = PID && findWindowID(PID);
+      const windowState = windowID && getWindowState(windowID);
+      processes.sessions[sessionID] = {
+        ...toSession(processes.sessions, sessionID),
+        portForward: { PID, windowID, windowState }
+      };
     }
     return processes;
-  }, { service: null, main: null, sessions: [] });
+  }, { service: null, main: null, sessions: {} });
 }
 
 function findWindowID(PID) {
-  return execCommand(`xdotool search --pid ${PID} --onlyvisible`).trim();
+  return execCommand(`xdotool search --all --pid ${PID} --onlyvisible --limit 1`).trim();
 }
 
 function getWindowState(windowID) {
-  const stdout = execCommand(`xprop -id ${windowID}`).trim();
+  const stdout = execCommand(`xprop -id ${windowID}`);
   return stdout.split('\n').reduce((windowState, line) => {
     const matches = WINDOW_STATE_REGEXP.exec(line);
-    return (matches && matches.groups.state) || windowState;
+    return (matches && matches.groups.state.trim()) || windowState;
   }, null);
 }
 
 function activateWindow(windowID) {
-  console.log(`RustDesk is minized: activating ${windowID}`);
-  execCommand(`xdotool windowactivate ${windowID}`);
+  GLib.spawn_command_line_async(`xdotool windowactivate ${windowID}`);
+}
+
+function startApp() {
+  GLib.spawn_command_line_async('rustdesk');
+}
+
+function exitApp(PID) {
+  GLib.spawn_command_line_async(`kill -SIGQUIT ${PID}`);
 }
 
 function startService() {
@@ -102,12 +138,8 @@ function restartService() {
   GLib.spawn_command_line_async('systemctl restart rustdesk');
 }
 
-function startApp() {
-  GLib.spawn_command_line_async('rustdesk');
-}
-
-function exitApp(PID) {
-  GLib.spawn_command_line_async(`kill -SIGQUIT ${PID}`);
+function startSession(action, sessionID) {
+  GLib.spawn_command_line_async(`rustdesk --${action} ${sessionID}`);
 }
 
 const Indicator = GObject.registerClass(
@@ -121,18 +153,20 @@ const Indicator = GObject.registerClass(
 
     updateIcon() {
       const offline = !this.rustDeskService.service;
-      const online = this.rustDeskService.sessions.length > 0;
+      const online = Object.keys(this.rustDeskService.sessions).length > 0;
       this.destroy_all_children();
       this.icon = new St.Icon({ style_class: `rustdesk-icon${online ? ' online' : ''}${offline ? ' offline' : ''}` });
       this.add_child(this.icon);
     }
 
-    updateMenu() {
-      console.log('updateMenu');
+    toSessionLabel(sessionID) {
+      return sessionID.split('').reverse().map((n, i) => `${(i + 1) % 3 === 0 ? ' ' : ''}${n}`).reverse().join('').trim();
+    }
 
+    updateMenu() {
       const service = this.rustDeskService.service;
       const main = this.rustDeskService.main;
-      const sessions = this.rustDeskService.sessions.filter(s => !s.deleted);
+      const sessions = Object.values(this.rustDeskService.sessions).filter(s => !s.deleted);
 
       this.menu.removeAll();
 
@@ -142,21 +176,35 @@ const Indicator = GObject.registerClass(
 
       if (sessions.length > 0) {
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-        sessions.forEach(({ PID, sessionID, windowID }) => {
-          const sessionSubMenu = new PopupMenu.PopupSubMenuMenuItem(sessionID);
-          const showSessionItem = new PopupMenu.PopupMenuItem('Show');
-          showSessionItem.connect('activate', () => activateWindow(windowID));
-          sessionSubMenu.menu.addMenuItem(showSessionItem);
-          const closeSessionItem = new PopupMenu.PopupMenuItem('Close');
-          closeSessionItem.connect('activate', () => exitApp(PID));
+        sessions.forEach(({ sessionID, connect, fileTransfer, portForward }) => {
+          const sessionSubMenu = new PopupMenu.PopupSubMenuMenuItem(this.toSessionLabel(sessionID));
+          const connectSessionItem = new PopupMenu.PopupMenuItem(gettext('Connect'));
+          if (connect.PID) {
+            const connectCloseButton = new St.Button({
+              style_class: 'menu-button window-close',
+              icon_name: 'window-close-symbolic',
+              x_align: Clutter.ActorAlign.END
+            });
+            connectCloseButton.connect('clicked', () => exitApp(connect.PID));
+            connectSessionItem.add_child(connectCloseButton);
+          }
+          connectSessionItem.connect('activate', () => connect.windowID ? activateWindow(connect.windowID) : startSession('connect', sessionID));
+          sessionSubMenu.menu.addMenuItem(connectSessionItem);
+          const fileTransferSessionItem = new PopupMenu.PopupMenuItem(gettext('Transfer File'));
+          fileTransferSessionItem.connect('activate', () => fileTransfer.windowID ? activateWindow(fileTransfer.windowID) : startSession('file-transfer', sessionID));
+          sessionSubMenu.menu.addMenuItem(fileTransferSessionItem);
+          const portForwardSessionItem = new PopupMenu.PopupMenuItem(gettext('TCP Tunneling'));
+          portForwardSessionItem.connect('activate', () => portForward.windowID ? activateWindow(portForward.windowID) : startSession('port-forward', sessionID));
+          sessionSubMenu.menu.addMenuItem(portForwardSessionItem);
+          sessionSubMenu.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+          const closeSessionItem = new PopupMenu.PopupMenuItem(gettext('Close session'));
+          closeSessionItem.connect('activate', () => [connect, fileTransfer, portForward].forEach(({ PID }) => PID && exitApp(PID)));
           sessionSubMenu.menu.addMenuItem(closeSessionItem);
           this.menu.addMenuItem(sessionSubMenu);
         });
-
         if (sessions.length > 1) {
-          const closeAll = new PopupMenu.PopupMenuItem(gettext('Close all'));
-          closeAll.connect('activate', () => sessions.forEach(({ PID }) => exitApp(PID)));
+          const closeAll = new PopupMenu.PopupMenuItem(gettext('Close all sessions'));
+          closeAll.connect('activate', () => sessions.forEach(({ connect: { PID } }) => exitApp(PID)));
           this.menu.addMenuItem(closeAll);
         }
       }
@@ -189,7 +237,15 @@ class RustDeskService {
   constructor() {
     this.service = null;
     this.main = null;
-    this.sessions = [];
+    this.sessions = {};
+  }
+
+  resolveSession(existingSession, session) {
+    delete session.deleted;
+    return ['connect', 'fileTransfer', 'portForward'].reduce((session, window) => {
+      if (existingSession[window].windowID !== session[window].windowID) session.changed = true;
+      return session;
+    }, session);
   }
 
   set(data) {
@@ -203,24 +259,29 @@ class RustDeskService {
     const mainClosed = previousMain && !data.main;
     this.main = data.main;
 
-    const previousSessions = JSON.parse(JSON.stringify(this.sessions))
-      .filter(s => !s.deleted)
-      .map(s => {
-        delete s.added;
-        s.deleted = true;
-        return s;
-      });
-    const sessions = data.sessions.reduce((sessions, session) => {
-      const existingSession = sessions.find(({ sessionID }) => sessionID === session.sessionID);
+    const previousSessions = JSON.parse(JSON.stringify(this.sessions));
+    Object.keys(previousSessions).forEach(sessionID => {
+      const previousSession = previousSessions[sessionID];
+      delete previousSessions[sessionID].added;
+      delete previousSessions[sessionID].changed;
+      if (previousSession.deleted) {
+        delete previousSessions[sessionID];
+        return;
+      }
+      previousSessions[sessionID].deleted = true;
+    });
+    const sessions = Object.values(data.sessions).reduce((sessions, session) => {
+      const existingSession = sessions[session.sessionID];
       if (existingSession) {
-        delete existingSession.deleted;
+        sessions[session.sessionID] = this.resolveSession(existingSession, session);
         return sessions;
       }
-      return [...sessions, { ...session, added: true }];
+      sessions[session.sessionID] = { ...session, added: true };
+      return sessions;
     }, previousSessions);
     this.sessions = sessions;
 
-    this.changes = serviceStarted || serviceStopped || mainStarted || mainClosed || sessions.filter(s => s.added || s.deleted).length > 0;
+    this.changes = serviceStarted || serviceStopped || mainStarted || mainClosed || Object.values(sessions).filter(s => s.added || s.deleted || s.changed).length > 0;
   }
 }
 
@@ -251,8 +312,9 @@ class Extension {
 
   refresh() {
     this.rustDeskService.set(parseProcesses());
-    if (this.indicator && !this.rustDeskService.main && this.rustDeskService.sessions.length === 0) this.removeIndicator();
-    if (!this.indicator && (this.rustDeskService.main || this.rustDeskService.sessions.length > 0)) this.addIndicator();
+    const sessionCount = Object.keys(this.rustDeskService.sessions).length;
+    if (this.indicator && !this.rustDeskService.main && sessionCount === 0) this.removeIndicator();
+    if (!this.indicator && (this.rustDeskService.main || sessionCount > 0)) this.addIndicator();
     if (this.indicator) {
       this.indicator.updateIcon();
       if (this.rustDeskService.changes) this.indicator.updateMenu();
